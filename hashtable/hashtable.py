5777562ab232037d9561dbed15a2ca42edda91e6
Steven Wang <stevenwang2060@gmail.com>
2020-08-03 15:45:53 -0700

    Finished day 1 MVP

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -22,6 +22,11 @@ class HashTable:
 
     def __init__(self, capacity):
         # Your code here
+        if capacity > MIN_CAPACITY:
+            self.capacity = capacity
+        else: 
+            self.capacity = MIN_CAPACITY
+        self.table = [None] * capacity
 
 
     def get_num_slots(self):
@@ -63,6 +68,10 @@ class HashTable:
         Implement this, and/or FNV-1.
         """
         # Your code here
+        hash = 5281
+        for b in key:
+            hash = ((hash << 5) + hash) + ord(b)
+        return hash
 
 
     def hash_index(self, key):
@@ -82,6 +91,8 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        index = self.hash_index(key)
+        self.table[index] = value
 
 
     def delete(self, key):
@@ -93,6 +104,8 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        index = self.hash_index(key)
+        self.table[index] = None
 
 
     def get(self, key):
@@ -104,6 +117,8 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        index = self.hash_index(key)
+        return self.table[index]
 
 
     def resize(self, new_capacity):
