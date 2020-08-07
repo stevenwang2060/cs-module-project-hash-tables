2eb24894c30dc44a23249fa9149c2db6cc7df148
Steven Wang <stevenwang2060@gmail.com>
2020-08-06 19:15:31 -0700

    Finished day 4 MVP

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -2,4 +2,33 @@
 # decode it.
 
 # Your code here
+cipher_text = open('applications/crack_caesar/ciphertext.txt').read()
+alphabet = 'ETAOHNRISDLWUGFBMYCPKVQJXZ'
 
+def get_frequency(text):
+    text = text.upper()
+    letter_freq = {}
+
+    for letter in alphabet:
+        letter_freq[letter] = 0
+    for letter in text:
+        if letter in alphabet:
+            letter_freq[letter] += 1
+    return letter_freq
+
+freq = get_frequency(cipher_text)
+frequency_list = ''.join(sorted(freq, key=lambda k: freq[k], reverse=True))
+print(frequency_list)
+
+def decrypt(text):
+    plain_txt = ''
+    for c in text:
+        index = frequency_list.find(c)
+        if alphabet[index] == 'Z':
+            plain_txt += ' '
+        else:
+            plain_txt += alphabet[index]
+    return plain_txt
+
+
+print(decrypt(cipher_text))
--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,19 @@
 # Your code here
+import re
+dic = {}
+text = open('applications/histo/robin.txt').read()
 
+def count_w(str):
+    global dic
+    filtered = re.sub(r"[^\w\d'\s]", '', str)
+    for w in filtered.lower().split():
+        if w not in dic.keys():
+            dic[w] = '#'
+        else:
+            dic[w] += '#'
+    return dic
+
+result = count_w(text)
+
+for word in sorted(result, key=lambda word: len(result[word]), reverse=True):
+    print(word, result[word])
